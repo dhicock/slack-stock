9004a1496f7455fe1ff98542ed4e19802c933ed7
dhicock@ahtenahealth.com <dhicock@athenahealth.com>
2017-05-08 13:07:53 -0500

    added challenge

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var pg = require('pg');
+var request = require('request');
+var bodyParser = require('body-parser');
+var SlackClient = require('@slack/client').WebClient;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.post('/stock', function(req, res){
+	res.status(200);
+	if(res.body.challenge){
+		res.send(challenge);
+		res.end();
+	}
+})
