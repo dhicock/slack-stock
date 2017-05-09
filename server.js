2984f39683b1ed9cb1957823c3c1f3dfb79f5c53
dhicock@ahtenahealth.com <dhicock@athenahealth.com>
2017-05-09 15:32:59 -0500

    added api

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8081;
 var token = process.env.SLACK_API_TOKEN;
 var regexPattern = /\$[A-Za-z]+/g;
+var apiUrl = 'http://finance.google.com/finance/info?client=ig&q=NASDAQ%3A';
 
 app.post('/stock', function(req, res){
 	res.status(200);
@@ -19,7 +20,7 @@ app.post('/stock', function(req, res){
 	}
 	var text = req.body.event.text;
 	if(!text || text.len == 0){
-		console.log('no text found');
+		//console.log('no text found');
 		res.end();
 		return;
 	}
@@ -28,14 +29,97 @@ app.post('/stock', function(req, res){
 		console.log('array: %s | len: ' + stockArr.length, stockArr);
 	}
 	if(!stockArr || stockArr.length == 0){
-		console.log('no stocks found');
+		//console.log('no stocks found');
 		res.end();
 		return;
 	}
 	var channel = req.body.event.channel;
-	res.send(text);
+	var symbols = [];
+	stockArr.forEach(function(element) {
+		symbols.push(element.substring(1));
+	});
+	var tickers = symbols.join(',');
+	var url = apiUrl + tickers;
+	request(url, function(error, response, body){
+		if(error){
+			console.log('error googleapi=%s', error);
+			res.status(500).end();
+			return;
+		}
+		if(response){
+			var json = JSON.parse(response.body.replace('//', ''));
+			var formattedJson = formatForSlack(json);
+			formattedJson['channel']=channel;
+			console.log(formattedJson);
+			var web = new SlackClient(token);
+			web.chat.postMessage(channel, '', formattedJson, function(err, res){
+				if(err){
+					console.log('Error: ' + err);
+				}else {
+					console.log('Message Sent: ', res);
+				}
+			});
+		}
+	})
 	res.end();
 })
+
+function formatForSlack(json, response_type){
+	var formattedJson = {};
+	formattedJson['as_user'] = false;
+	formattedJson['attachments'] = [];
+	formattedJson['response_type'] = response_type || 'in_channel';
+	formattedJson['footer'] = 'Data from Google Finance';
+	json.forEach(function(element){
+		var attachment = {};
+		var change = element.c;
+		var changePerc = element.cp;
+		var price = element.l;
+		var afterHoursPrice = element.el;
+		var ticker = element.t;
+
+		var imgUrl = 'http://markets.money.cnn.com/services/api/chart/snapshot_chart_api.asp?symb=' + element.t;
+
+		if(changePerc >= 0){
+			attachment['color'] = 'good';
+		} else{
+			attachment['color'] = 'danger';
+		}
+
+		attachment['fields'] = [
+			{
+				"title": "Ticker",
+				"value": ticker,
+				"short": true
+			},
+			{
+				"title": "Current Price",
+				"value": price,
+				"short": true
+			},
+			{
+				"title": "Change",
+				"value": change,
+				"short": true
+			},
+			{
+				"title": "Percent Change",
+				"value": changePerc,
+				"short": true
+			}
+		];
+		if(afterHoursPrice){
+			attachment['fields'].push({
+				"title": "After Hours Price",
+				"value": afterHoursPrice,
+				"short":true
+			});
+		}
+		attachment["image_url"] = imgUrl;
+		formattedJson.attachments.push(attachment);
+	});
+	return formattedJson;
+}
 
 var server = app.listen(port, function() {
 	var port = server.address().port;
