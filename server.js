1ae3c642fbe733c7a83beaaecc4356310de0dcca
dhicock@ahtenahealth.com <dhicock@athenahealth.com>
2017-05-09 14:06:31 -0500

    testing

--- server.js
+++ server.js
@@ -7,14 +7,28 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8081;
+var token = process.env.SLACK_API_TOKEN;
+var regexPattern = new RegExp(/\$[A-Za-z]+/);
 
 app.post('/stock', function(req, res){
 	res.status(200);
 	console.log('Message received!\nMessage: '+JSON.stringify(req.body));
 	var challenge = req.body.challenge;
 	if(challenge){
-		res.send(challenge).end();
+		res.send(challenge);
 	}
+	var text = req.body.text;
+	if(!text || text.len == 0){
+		res.end();
+		return;
+	}
+	var stockArr = text.match(regexPattern);
+	if(!stockArr || stockArr.len == 0){
+		res.end();
+		return;
+	}
+	var channel = req.body.channel;
+	res.send(stockArr);
 	res.end();
 })
 
