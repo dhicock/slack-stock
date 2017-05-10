821b723f5084d686249678bad8c61ee85e6d2cc6
dhicock@ahtenahealth.com <dhicock@athenahealth.com>
2017-05-10 13:10:05 -0500

    wtf is happening

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/stock', function(req, res){
 	var ts = req.body.event.thread_ts;
 	var symbols = [];
 	stockArr.forEach(function(element) {
-		symbols.push(element.replace('$$', ''));
+		symbols.push(element.replace(/\$+/, ''));
 	});
 	var tickers = symbols.join(',');
 	if(dhicock){
